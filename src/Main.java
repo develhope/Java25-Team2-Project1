fe5ff3aa6eb1e99527c83b26c4b68be369e6d7b7
Giuseppe <giuseppe.riccobono@hotmail.com>
2024-04-08 14:26:31 +0200

    Divisione-Potenza

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,25 @@ public class Main {
         return x * y;
     }
 
+    public static  int divisionValues (int x, int y) {
+        if (y == 0) {
+            System.out.print("ERRORE. Impossibile dividere per ");
+            return y;
+        } else {
+            System.out.println("Resto: " + (x % y));
+            return x / y;
+        }
+    }
+
+    public static int expoValues (int x, int y) {
+        int exp = 1;
+        for (int i = 0; i < y; i++) {
+            exp = exp * x;
+        }
+        return exp;
+    }
+
     public static boolean checkValue (int x) {
         return (x % 2 == 0) && (x != 0);
     }
-
-
 }
