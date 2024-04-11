6874923d26f1034432749237bcd4bbad343f6d70
AndreaVecchione <164344505+AndreaVecchione@users.noreply.github.com>
2024-04-11 17:27:23 +0200

    Update test.java
    Moltiplicazione e sottrazione

--- src/test.java
+++ src/test.java
@@ -2,7 +2,10 @@ public class test {
     public static void main(String[] args) {
 
         double[] arrValues = {4, 6, 2, 5, 7};
+        int x = 5;
         char operator = '+';
+        System.out.println(("Il Numero inserito è: " + x));
+        System.out.println("Il numero è pari: " + checkNumber(x));
 
         switch (operator) {
             case '+':
@@ -11,11 +14,11 @@ public class test {
                 break;
             case '-':
                 System.out.println("Operazione di Sottrazione");
-                System.out.println();
+                System.out.println(sottrazione(arrValues));
                 break;
             case '*':
                 System.out.println("Operazione di Moltiplicazione");
-                System.out.println();
+                System.out.println(moltiplicazione(arrValues));
                 break;
             case '/':
                 System.out.println("Operazione di Divisione");
@@ -30,6 +33,20 @@ public class test {
         double result = 0;
         for (int i = 0; i < x.length; i++) {
             result = result + x[i];
+        }
+        return result;
+    }
+     public static int sottrazione(int[] x) {
+        int result = 0;
+        for (int i = 0; i < x.length; i++) {
+            result = result - x[i];
+        }
+        return result;
+    }
+    public static int moltiplicazione(int[] x) {
+        int result = 1;
+        for (int i = 1; i < x.length; i++) {
+            result = result * x[i];
         }
         return result;
     }
